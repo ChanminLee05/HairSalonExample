97da3a7a7d614ef28578d6ae926eded7ee3fc488
Chanmin Lee <lee00837@algonquinlive.com>
2024-10-23 10:21:36 -0600

    added mobile size

--- src/Components/Page/Page.js
+++ src/Components/Page/Page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "./Page.css";
 import Cover from "../../Assets/salon-cover.jpg"
 import Navbar from "../Nav/Navbar";
@@ -7,36 +7,82 @@ import HairPerm from "../../Assets/perm.png";
 import HairColor from "../../Assets/hair-dye.png";
 import About from "../../Assets/about.jpg";
 
-import {Link} from "react-router-dom";
 const Page = () => {
+    const coverRef = useRef(null);
+    const aboutRef = useRef(null);
+    const serviceRef = useRef(null);
+    const [visibleSection, setVisibleSection] = useState({
+        cover: false,
+        about: false,
+        service: false,
+    });
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const cover = coverRef.current;
+            const about = aboutRef.current;
+            const service = serviceRef.current;
+
+            const checkVisibility = (ref) => {
+                if (ref) {
+                    const rect = ref.getBoundingClientRect();
+                    return rect.top < window.innerHeight * 0.75 && rect.bottom > window.innerHeight * 0.25;
+                }
+                return false;
+            };
+
+            setVisibleSection({
+                cover: checkVisibility(cover),
+                about: checkVisibility(about),
+                service: checkVisibility(service),
+            });
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className="salon-page">
             <Navbar />
-            <div className="cover-section">
+            <div
+                className={`cover-section ${visibleSection.cover ? 'animate' : ''}`}
+                ref={coverRef}
+            >
                 <div className="cover-txt">
                     <h1>Make Your Style</h1>
                     <h1>Best Salon in Edmonton</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aut harum in totam
-                        voluptatibus. Alias aliquam, commodi deleniti dignissimos dolor eum facere, fugiat fugit,
-                        inventore perferendis perspiciatis provident repellendus tempore!
+                        voluptatibus. Alias aliquam!
                     </p>
                     <a href="https://examplesite.trafft.com" target="_blank"><i className="fa-regular fa-calendar-check"></i>Book Online</a>
                 </div>
                 <img className="cover-img" src={Cover} alt="" />
             </div>
-            <div className="about-section" id="about">
+            <div
+                className={`about-section ${visibleSection.about ? 'animate' : ''}`}
+                id="about"
+                ref={aboutRef}
+            >
                 <h1>About Us</h1>
                 <div className="about-us">
                     <img src={About} alt="" className="about-img"/>
                     <div className="about-txt">
                         <h1>Korean Hair Dresser</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dignissimos exercitationem
-                            inventore tenetur! Aliquid, asperiores consequatur esse ex expedita laboriosam magnam magni
-                            minima nam, non odit ratione sequi tempora voluptatibus!</p>
+                            inventore tenetur! Aliquid!</p>
                     </div>
                 </div>
             </div>
-            <div className="service-section" id="service">
+            <div
+                className={`service-section ${visibleSection.service ? 'animate' : ''}`}
+                id="service"
+                ref={serviceRef}
+            >
                 <h1>Services</h1>
                 <div className="hair-service-container">
                     <div className="hair-service hair-cut">
